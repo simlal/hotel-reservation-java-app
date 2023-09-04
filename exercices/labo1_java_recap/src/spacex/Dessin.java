2e20d0b6737291004c3f43d9cb666538e64e5af1
Simon Lalonde <simlalonde@hotmail.com>
2023-09-04 19:40:23 -0400

    Add main draw class

---
+++ exercices/labo1_java_recap/src/spacex/Dessin.java
@@ -0,0 +1,12 @@
+package spacex;
+
+public class Dessin {
+    public static void main (String[] args) {
+        System.out.println("  /\\  ");
+        System.out.println(" /__\\  ");
+        System.out.println(" |  | ");
+        System.out.println("/____\\");
+        System.out.println(" /WW\\ ");
+    }
+
+}
