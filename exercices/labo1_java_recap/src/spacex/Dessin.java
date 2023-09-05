9ab3dd2bfad0c45a08d59367c56be46536ecc373
Simon Lalonde <simlalonde@hotmail.com>
2023-09-04 20:13:29 -0400

    ex2-3 dynamic print method Citerne

--- exercices/labo1_java_recap/src/spacex/Dessin.java
+++ exercices/labo1_java_recap/src/spacex/Dessin.java
@@ -2,11 +2,31 @@ package spacex;
 
 public class Dessin {
     public static void main (String[] args) {
-        System.out.println("  /\\  ");
-        System.out.println(" /__\\  ");
-        System.out.println(" |  | ");
-        System.out.println("/____\\");
-        System.out.println(" /WW\\ ");
+        Dessin fusee = new Dessin();
+        
+        String resultat = fusee.dessinerCargo();
+        resultat += fusee.dessinerCiterne(5);
+        resultat += fusee.dessinerMoteur();
+        System.out.println(resultat);
     }
 
+    private String dessinerCargo() {
+        String cargo = "  /\\  \n" + " /__\\  \n";
+        return cargo;
+    }
+
+    private String dessinerCiterne(int nombreCiternes) {
+        String citerne = "";
+        for (int i = 0; i < nombreCiternes; i++) {
+            citerne += " |  | \n";
+        }
+        citerne += "/____\\\n";
+
+        return citerne;
+    }
+
+    private String dessinerMoteur() {
+        String moteur = " /WW\\ ";
+        return moteur;
+    }
 }
