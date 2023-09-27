326a9148a28f6c8eae451400e5f2f84ad57f2158
Simon Lalonde <simlalonde@hotmail.com>
2023-09-27 15:59:06 -0400

    Init commit all tables creation sql script

---
+++ tps/tp2/TP2/creation.sql
@@ -0,0 +1,39 @@
+-- Table Client
+CREATE TABLE Client (
+    idClient int PRIMARY KEY,
+    prenom varchar(255) NOT NULL,
+    nom varchar(255) NOT NULL,
+    age int CHECK(age > 0)
+);
+
+-- Table Chambre
+CREATE TABLE Chambre(
+    idChambre int PRIMARY KEY,
+    typeLit varchar(255) NOT NULL,
+    prixBase float8 CHECK(prixBase > 0) NOT NULL
+);
+
+-- Table Reservation
+CREATE TABLE Reservation(
+    idReservation int PRIMARY KEY,
+    dateDebut date NOT NULL,
+    dateFin date NOT NULL,
+    idClient int REFERENCES Client(idClient),
+    idChambre int REFERENCES Chambre(idChambre)
+);
+
+-- Table Commodite
+CREATE TABLE Commodite(
+    idCommodite int PRIMARY KEY,
+    description varchar(255),
+    surplusPrix float8 CHECK(surplusPrix > 0) NOT NULL
+);
+
+-- Table ChambreCommodite
+CREATE TABLE ChambreCommodite(
+    idChambre int,
+    idCommodite int,
+    PRIMARY KEY (idChambre, idCommodite),
+    FOREIGN KEY (idChambre) REFERENCES Chambre(idChambre),
+    FOREIGN KEY (idCommodite) REFERENCES Commodite(idCommodite)
+);
