d159ceb98a317181b6609b645eace1431563b057
Simon Lalonde <simlalonde@hotmail.com>
2023-09-27 16:11:16 -0400

    Add destruction.sql init commit

---
+++ tps/tp2/TP2/destruction.sql
@@ -0,0 +1,14 @@
+-- Drop ChambreCommodite
+DROP TABLE IF EXISTS ChambreCommodite CASCADE;
+
+-- Drop Reservation
+DROP TABLE IF EXISTS Reservation CASCADE;
+
+-- Drop Chambre
+DROP TABLE IF EXISTS Chambre CASCADE;
+
+-- Drop Commodite
+DROP TABLE IF EXISTS Commodite CASCADE;
+
+-- Drop Client
+DROP TABLE IF EXISTS Client CASCADE;
