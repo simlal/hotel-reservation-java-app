15c15d70debf84dc15f36f5c4f485745ae2e6df3
simlal <simlalonde@hotmail.com>
2023-11-13 21:04:40 -0500

    Add unsaved tuples

--- hotel-res-app/mongodb-app/AubergeInn/src/main/java/com/ift287lalonde/TupleChambre.java
+++ hotel-res-app/mongodb-app/AubergeInn/src/main/java/com/ift287lalonde/TupleChambre.java
@@ -1,4 +1,4 @@
-package Tuples;
+package com.ift287lalonde;
 
 import java.util.List;
 import java.util.ArrayList;
--- hotel-res-app/mongodb-app/AubergeInn/src/main/java/com/ift287lalonde/TupleCommodite.java
+++ hotel-res-app/mongodb-app/AubergeInn/src/main/java/com/ift287lalonde/TupleCommodite.java
@@ -1,4 +1,4 @@
-package Tuples;
+package com.ift287lalonde;
 
 import java.util.ArrayList;
 import java.util.List;
