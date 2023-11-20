676232716e9413f6083a634892444ffc1eee4376
Simon Lalonde <simlalonde@hotmail.com>
2023-11-19 19:41:37 -0500

    remove serial

--- hotel-res-app/mongodb-app/AubergeInn/src/main/java/com/ift287lalonde/IFT287Exception.java
+++ hotel-res-app/mongodb-app/AubergeInn/src/main/java/com/ift287lalonde/IFT287Exception.java
@@ -1,6 +1,6 @@
 package com.ift287lalonde;
 
-import java.io.Serial;
+// import java.io.Serial;
 
 /**
  * L'exception BiblioException est levée lorsqu'une transaction est inadéquate.
@@ -8,8 +8,8 @@ import java.io.Serial;
  */
 public final class IFT287Exception extends Exception
 {
-	@Serial
-	private static final long serialVersionUID = 1L;
+	// @Serial
+	// private static final long serialVersionUID = 1L;
 
 	public IFT287Exception(String message)
 	{
